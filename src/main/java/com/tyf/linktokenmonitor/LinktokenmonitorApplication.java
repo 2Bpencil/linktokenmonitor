78145cf489d745e416ba5f28a61ee2ba5004412e
tyf <63250124@qq.com>
2019-07-05 16:33:20 +0800

    .

--- src/main/java/com/tyf/linktokenmonitor/LinktokenmonitorApplication.java
+++ src/main/java/com/tyf/linktokenmonitor/LinktokenmonitorApplication.java
@@ -8,6 +8,9 @@ public class LinktokenmonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LinktokenmonitorApplication.class, args);
+
+        System.out.println("aaa");
+
     }
 
 }
